e697f5ffa3a59d918bdbb457d562239f043bf447
marijkescotuzzi <m.scotuzzi@gmail.com>
2016-10-26 09:14:51 +0000

    Ho fatto un bel lavoro

--- script.py
+++ script.py
@@ -1,2 +1,13 @@
 print('Hello World')
 print('I like this course')
+print('')
+
+import test2 as cir
+
+r = 10
+c = cir.circ(r)
+a = cir.area(r)
+print('')
+print('The radisu is',r)
+print('The circumference is',c)
+print('The area is', a)
